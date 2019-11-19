0e46c3cc3f4f221b3da43dd0e178cac5b5cbd179
bibin <bibinthomas787@gmail.com>
2019-11-19 16:58:11 -0500

    committed by adding the first view

--- musicPlayerApp/FirstViewController.swift
+++ musicPlayerApp/FirstViewController.swift
@@ -8,13 +8,85 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+
+var songs:[String] = []
+class FirstViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gettingSongsName()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+       {
+           let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+           cell.textLabel?.text = songs[indexPath.row]
+           return cell
+       }
+       
+    
+    func gettingSongsName(){
+        
+        
+        let folderURL = URL(fileURLWithPath: Bundle.main.resourcePath!)
+        
+        
+        do {
+            
+            let songpath =  try FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+           
+           
+           
+            for song in songpath  {
+                
+                var mysong = song.absoluteString
+               
+               
+               
+                
+                if mysong.contains(".mp3")
+                
+                {
+                
+                    let findString  = mysong.components(separatedBy: "/")
+                    
+                    print(findString[findString.count-1])
+                   
+                    
+                    mysong = mysong.replacingOccurrences(of: "%20", with: " ")
+                   
+                    mysong = mysong.replacingOccurrences(of: ".mp3", with: " ")
+                    
+                    songs.append(mysong)
+                    
+                }
+            }
+            print(songs)
+            myTableView.reloadData()
+            
+        }
+        
+        catch
+        {
+            
+            
+            
+        }
+    }
 
 }
 
