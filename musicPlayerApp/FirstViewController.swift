649cca486018e9c6227a1e72552a1a0dce00b17d
bibin <bibinthomas787@gmail.com>
2019-11-19 15:31:48 -0500

    Initial Commit

---
+++ musicPlayerApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  musicPlayerApp
+//
+//  Created by MacStudent on 2019-11-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ musicPlayerApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  musicPlayerApp
+//
+//  Created by MacStudent on 2019-11-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
